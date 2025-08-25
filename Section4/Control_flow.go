c871489bccada9e184b512fca90738006719506e
Dinesh Kolla <100382566+Kolla1997@users.noreply.github.com>
2025-08-24 21:34:56 -0500

    adding section-4

---
+++ Section4/Control_flow.go
@@ -0,0 +1,46 @@
+package Section4
+
+import "fmt"
+
+func If_else(temp float32) {
+	if temp < 30.0 {
+		fmt.Println("Todays weather is super cold!")
+	} else if temp > 30.0 && temp <= 50.0 {
+		fmt.Println("Todays weather is cold!")
+	} else if temp > 50.0 && temp <= 70.0 {
+		fmt.Println("Todays weather is moderate!")
+	} else {
+		fmt.Println("Todays weather is hot!")
+	}
+}
+
+func Switch_case(day int) {
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
+	default:
+		fmt.Println("Please eneter the valid day number (1-7)")
+	}
+}
+
+func ControlFlow(limit int) {
+	for i := 1; i <= limit; i++ {
+		if i%6 == 0 {
+			fmt.Print("\n", i, " ")
+		} else {
+			fmt.Print(i, " ")
+		}
+	}
+}
--- main.go
+++ main.go
@@ -2,10 +2,35 @@ package main
 
 import (
 	"GoLang_Class/Section1"
+	"GoLang_Class/Section4"
+
 	"fmt"
 )
 
 func main() {
+	var temp float32
+	var day int
+	var limit int
+	fmt.Scanln("Please enter the day number (1-7):", &day)
 	fmt.Println("Hello World")
 	Section1.Variables()
+	fmt.Print("Please enter the temperature:")
+	if _, err := fmt.Scanln(&temp); err == nil {
+		Section4.If_else(temp)
+	} else {
+		fmt.Println("Error in reading the temperature:", err)
+	}
+	fmt.Print("Please enter the day number (1-7):")
+	if _, err := fmt.Scanln(&day); err == nil {
+		Section4.Switch_case(day)
+	} else {
+		fmt.Println("Error in reading the day number:", err)
+	}
+	fmt.Print("Please eneter a number to all numbers upto that number:")
+	if _, err := fmt.Scanln(&limit); err == nil {
+		Section4.ControlFlow(limit)
+	} else {
+		fmt.Println("Error in reading the prime number:", err)
+	}
+
 }
